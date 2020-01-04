bb977fe9ac5aca093f38fad0a2c8bc3d72dcdc03
Peter Lin <peterlin5301997@gmail.com>
2020-01-04 15:57:55 -0500

    adds array datatypes

--- types.js
+++ types.js
@@ -1,4 +1,5 @@
 console.log('Hello World from TS');
+console.log('');
 // Basic DataTypes Declaration
 var myString;
 var myNum;
@@ -15,3 +16,18 @@ console.log('String:', myString);
 console.log('Number:', myNum);
 console.log('Boolean:', myBool);
 console.log('Any:', myVar);
+console.log('');
+// Array DataTypes Declaration
+var strArray; // same as 'let strArray: Array<string>'
+var numArray; // same as 'let numArray: Array<number>'
+var boolArray; // same as 'let boolArray: Array<boolean>'
+// Array DataTypes Assignment
+strArray = ['Hello', 'World'];
+numArray = [1, 2, 3];
+boolArray = [true, false, true];
+// Print Array DataTypes
+console.log('Array DataTypes');
+console.log('String Array:', strArray);
+console.log('Number Array:', numArray);
+console.log('Boolean Array:', boolArray);
+console.log('');
--- types.ts
+++ types.ts
@@ -1,4 +1,5 @@
-console.log('Hello World from TS')
+console.log('Hello World from TS');
+console.log('');
 
 // Basic DataTypes Declaration
 let myString: string;
@@ -18,3 +19,21 @@ console.log('String:', myString);
 console.log('Number:', myNum);
 console.log('Boolean:', myBool);
 console.log('Any:', myVar);
+console.log('');
+
+// Array DataTypes Declaration
+let strArray: string[];   // same as 'let strArray: Array<string>'
+let numArray: number[];   // same as 'let numArray: Array<number>'
+let boolArray: boolean[]; // same as 'let boolArray: Array<boolean>'
+
+// Array DataTypes Assignment
+strArray = ['Hello', 'World'];
+numArray = [1,2,3];
+boolArray = [true, false, true];
+
+// Print Array DataTypes
+console.log('Array DataTypes');
+console.log('String Array:', strArray);
+console.log('Number Array:', numArray);
+console.log('Boolean Array:', boolArray);
+console.log('');
