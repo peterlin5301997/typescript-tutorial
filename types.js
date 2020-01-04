7da6d0840cb508d75bcc5653aa9a5fcf107c7867
Peter Lin <peterlin5301997@gmail.com>
2020-01-04 15:55:02 -0500

    adds basic datatypes

--- types.js
+++ types.js
@@ -1,1 +1,17 @@
 console.log('Hello World from TS');
+// Basic DataTypes Declaration
+var myString;
+var myNum;
+var myBool;
+var myVar;
+// Basic DataTypes Assignment
+myString = 'Hello World';
+myNum = 22;
+myBool = true;
+myVar = 'I can be anything';
+// Print Basic DataTypes
+console.log('Basic DataTypes');
+console.log('String:', myString);
+console.log('Number:', myNum);
+console.log('Boolean:', myBool);
+console.log('Any:', myVar);
--- types.ts
+++ types.ts
@@ -1,1 +1,20 @@
 console.log('Hello World from TS')
+
+// Basic DataTypes Declaration
+let myString: string;
+let myNum: number;
+let myBool: boolean;
+let myVar: any;
+
+// Basic DataTypes Assignment
+myString = 'Hello World';
+myNum = 22;
+myBool = true;
+myVar = 'I can be anything';
+
+// Print Basic DataTypes
+console.log('Basic DataTypes');
+console.log('String:', myString);
+console.log('Number:', myNum);
+console.log('Boolean:', myBool);
+console.log('Any:', myVar);
