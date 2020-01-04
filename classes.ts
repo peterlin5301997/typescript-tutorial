09655e8416c4d0b894e6cb7d7c1b0a963ebde9b1
Peter Lin <peterlin5301997@gmail.com>
2020-01-04 16:55:36 -0500

    adds child class Member

--- classes.ts
+++ classes.ts
@@ -14,9 +14,31 @@ class User {
   register() {
     console.log(this.name + ' is now registered.');
   }
+
+  payInvoice() {
+    console.log(this.name + ' has paid invoice.')
+  }
 }
 
 // Instantiate New Class
 let john = new User('John Doe', 'jdoe@gmail.com', 34);
 
 john.register();
+
+// Child Class Member (Inherits from User Class)
+class Member extends User {
+  id: number;
+
+  constructor(id: number, name: string, email: string, age: number) {
+    super(name, email, age);
+    this.id = id;
+  }
+
+  payInvoice() {
+    super.payInvoice()
+  }
+}
+
+let mike: User = new Member(1, 'Mike Smith', 'mike@gmail.com', 33);
+
+mike.payInvoice();
