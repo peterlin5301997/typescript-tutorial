9daaaa723d3b91c01035d9a75693586b48b62a54
Peter Lin <peterlin5301997@gmail.com>
2020-01-04 16:29:59 -0500

    adds string function

--- functions.js
+++ functions.js
@@ -1,8 +1,9 @@
 console.log('');
-console.log('TypeScript Functions');
+// Number Functions
 function getSum(num1, num2) {
     return num1 + num2;
 }
+console.log('Number Functions');
 console.log('getSum(1,4):', getSum(1, 4));
 console.log('getSum function only takes in numbers and returns a number.');
 var mySum = function (num1, num2) {
@@ -17,3 +18,16 @@ var mySum = function (num1, num2) {
 };
 console.log('mySum(\'3\',5):', mySum('3', 5));
 console.log('mySum function takes in any (string or number) variable and returns a number.');
+console.log('');
+// String Functions
+function getName(firstname, lastname) {
+    if (lastname == undefined) {
+        return firstname;
+    }
+    return firstname + lastname;
+}
+console.log('String Function');
+console.log('getName(\'John\', \'Doe\'):', getName('John', 'Doe'));
+console.log('getName(\'John\'):', getName('John'));
+console.log('getName function returns fullname (firstname + lastname) but also returns just the firstname if there is no lastname');
+console.log('');
--- functions.ts
+++ functions.ts
@@ -1,10 +1,11 @@
 console.log('');
-console.log('TypeScript Functions');
 
+// Number Functions
 function getSum(num1:number, num2:number):number {
   return num1 + num2;
 }
 
+console.log('Number Functions');
 console.log('getSum(1,4):', getSum(1,4));
 console.log('getSum function only takes in numbers and returns a number.');
 
@@ -23,3 +24,19 @@ let mySum = function(num1:any, num2:any): number {
 
 console.log('mySum(\'3\',5):', mySum('3',5));
 console.log('mySum function takes in any (string or number) variable and returns a number.')
+console.log('');
+
+// String Functions
+function getName(firstname:string, lastname:string):string {
+  if (lastname == undefined) {
+    return firstname;
+  }
+
+  return firstname + lastname;
+}
+
+console.log('String Function');
+console.log('getName(\'John\', \'Doe\'):', getName('John', 'Doe'));
+console.log('getName(\'John\'):', getName('John'));
+console.log('getName function returns fullname (firstname + lastname) but also returns just the firstname if there is no lastname')
+console.log('');
